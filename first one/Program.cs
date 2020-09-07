86f3f7834da926248343c6e07133cd7a43f183c1
Timborg <timl@live.se>
2020-09-07 10:35:00 +0200

    int

---
+++ first one/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace first_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World like to code!");
+        }
+    }
+}
