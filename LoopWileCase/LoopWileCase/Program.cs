3f980ff3b64e82cd70f3213fae3b5d1935281c72
Tim Lindborg <timl@live.se>
2020-09-11 16:38:24 +0200

    FirsAssignment

---
+++ LoopWileCase/LoopWileCase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LoopWileCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            byte aByte = byte.MinValue; // 0 - 255
+            short = short.MinValue // -32k - 32k
+            int aInt = int.MinValue // -2.1B - 2.1BB
+            long aLong = long.MinValue; //
+
+            float aFloat = float.MaxValue; // power of 38
+            double aDouble = double.MaxValue; //power of 308
+            decimal aDecimal = decimal.MaxValue; // freaking big
+
+            Console.WriteLine(1 / 3);
+
+            // float/double/decimal
+            Console.WriteLine(1.0/3.0)
+
+                char aChar =  // asymbol a ascii table number 97
+                // alt key + arrow key up/down
+
+
+            Console.WriteLine(aFloat); //
+            Console.WriteLine(aByte);
+        }
+    }
+}
